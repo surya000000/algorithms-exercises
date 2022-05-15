eda0d331b4d8002dd9efaa3c708928b51e57a82f
surya <surya.siwakoti@impact.com>
2022-05-15 09:43:17 +0545

    Added bubble sort algorithm

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,12 +11,24 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+    let loop = true;
+    while(loop) {
+        loop = false;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i+1] < nums[i]) {
+                temp = nums[i]
+                nums[i] = nums[i+1]
+                nums[i+1] = temp
+                loop = true
+            }
+        }
+  }
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
