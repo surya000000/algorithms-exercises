6e4e85c4f6fa556782aedd39495891ae37461491
surya <surya.siwakoti@impact.com>
2022-05-16 10:23:39 +0545

    Pass nested array recursion test

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
   write a function that accepts an array that only contains
   two types of things: numbers and arrays. those nested arrays
@@ -6,14 +6,22 @@
 
   example: nestedAdd([1, 2, [3]]) = 6
            nestedAdd([[[2]], 1, [1, 3]]) = 7
- 
+
  */
 
 function nestedAdd(array) {
-  // write code here
+    let sum = 0;
+    array.forEach((item) => {
+        if (item.constructor.name === 'Array') {
+            sum += nestedAdd(item)
+        } else {
+            sum += item
+        }
+    })
+    return sum;
 }
 
-test.skip("nested arrays addition", () => {
+test("nested arrays addition", () => {
   expect(nestedAdd([1, 2, 3])).toEqual(6);
   expect(nestedAdd([1, [2], 3])).toEqual(6);
   expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
