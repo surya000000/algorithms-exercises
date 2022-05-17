960e629c75c56b30f917b28682c9a6f597c338af
surya <surya.siwakoti@impact.com>
2022-05-17 09:42:15 +0545

    Pass merge sort spec

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,33 @@
   You'll need to write more than just one function
 */
 
+const merge  = (lArray, rArray) => {
+    let results = [];
+
+    while(lArray.length && rArray.length) {
+        if (lArray[0] <= rArray[0]) {
+            results.push(lArray.shift())
+        } else {
+            results.push(rArray.shift())
+        }
+    }
+    return results.concat(lArray, rArray)
+}
+
 const mergeSort = (nums) => {
-  // code goes here
+    if (nums.length === 1) {
+        return nums;
+    }
+    const index = Math.floor(nums.length / 2)
+    const lArray = nums.slice(0, index)
+    const rArray = nums.slice(index)
+
+    return merge(mergeSort(lArray), mergeSort(rArray))
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
